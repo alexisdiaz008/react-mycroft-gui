1831ffb37873caacfad7e12cb3d518eb7e832be3
ralgernon <25943396+ralgernon@users.noreply.github.com>
2021-02-05 16:00:14 -0600

    refactor: Use consistent PascalCase for filenames

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import App from './components/app'
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById("root"))
