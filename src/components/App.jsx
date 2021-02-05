57fa165d4091a48fdea501bec17b416c032594b2
ralgernon <25943396+ralgernon@users.noreply.github.com>
2021-02-05 17:34:47 -0600

    refactor: Fix capitalization consistency

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Face from './face/face'
-import MycroftMessageBusSkillComponent from './mycroft_message_bus_elements/mycroft_message_bus';
+import Face from './face/Face'
+import MycroftMessageBusSkillComponent from './mycroft_message_bus_elements/MycroftMessageBus';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
+import './App.css';
 
-class App extends Component {	
+class App extends Component {
   render() {
     return (
       <div className="App container">
