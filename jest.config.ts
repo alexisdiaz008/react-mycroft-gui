c2213384b0275e725ab589dbe62b7ee09bf5dd75
ralgernon <25943396+ralgernon@users.noreply.github.com>
2021-02-05 14:56:56 -0600

    build: Configure Jest
    I set collectCoverage knowing good and well there are no tests in this
    repo. Nowhere to go but up!

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type {Config} from "@jest/types";
+
+// Sync object
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  roots: ["<rootDir>/src/"],
+  verbose: true,
+  transform: {
+    // process `*.ts` files with `ts-jest`
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+};
+
+export default config;
+
+
