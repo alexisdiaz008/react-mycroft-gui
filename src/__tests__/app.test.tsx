d6e1049219dd7b6820b194a6f9016c414b18af78
ralgernon <25943396+ralgernon@users.noreply.github.com>
2021-02-06 15:10:57 -0600

    test: Check if the App renders

---
+++ src/__tests__/app.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import App from './components/App';
+
+
+describe('<App />', () => {
+  test('renders the component', () => {
+    const component = shallow(<App />);  expect(component).toMatchSnapshot();
+  });
+});
