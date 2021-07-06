3981c5d5631c6cd306b4720dcaabcaf96c8611ac
alexisdiaz <lexmasta@gmail.com>
2021-07-05 23:52:20 -0400

    temp adjutant resting and active face

--- src/components/mycroft_message_bus_elements/widgets/face.jsx
+++ src/components/mycroft_message_bus_elements/widgets/face.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import activeFace from "./assets/images/restingfaceplaceholder.gif";
-import restingFace from "./assets/images/ai.gif";
+import activeFace from "./assets/images/adjutant_active.gif";
+import restingFace from "./assets/images/adjutant_resting.gif";
 import "./assets/css/face.css";
 
 export function Face(props) {
